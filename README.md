5c0a840b12f50479c7056baf2bec446e812575ad
strtab <nixjoyer@gmail.com>
2026-05-03 11:16:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+Material design circle plymouth theme for NixOS.
+
+Based on
+
+- [gevera/plymouth_themes](https://github.com/gevera/plymouth_themes).
+- [m3](https://m3.material.io/components/progress-indicators/specs).
+
+## Installation
+
+```nix
+# flake.nix
+inputs.plymouth-circle.url = "github:you/plymouth-circle";
+```
+
+```nix
+# nixosConfiguration
+imports = [ inputs.plymouth-circle.nixosModules.default ];
+
+boot.plymouthCircle = {
+  enable = true;
+  # logo = ./my-logo.png;
+  circle = {
+    red   = 98;
+    green = 114;
+    blue  = 164;
+    wavy  = true;
+  };
+};
+```
+
+## Options
+
+### `boot.plymouthCircle.enable`
+
+Enable the theme. Default: `false`.
+
+### `boot.plymouthCircle.logo`
+
+Path to a `.png` file used as the center logo.
+Default: `images/logo.png` from the flake.
+
+### `boot.plymouthCircle.circle.red` / `.green` / `.blue`
+
+RGB color of the animated circle. Each channel is an integer from `0` to `255`.
+Default: `10` for all channels.
+
+### `boot.plymouthCircle.circle.wavy`
+
+Use wavy-style circle frames (`wave/`) instead of the default ones (`normal/`).
+Default: `false`.
+
+## Repository layout
+
+```
+.
+├── flake.nix
+├── theme.plymouth
+├── theme.script
+└── images/
+    ├── logo.png
+    ├── normal/   # frames used when wavy = false
+    └── wave/     # frames used when wavy = true
+```
+
+### Installation on regular linux
+
+```sh
+git clone --depth 1 https://github.com/strtab/plymouth_material_design_circle-theme.git /tmp/plymouth_material_design_circle
+mkdir -p /usr/share/plymouth/themes/material_design_circle/
+cp -vr /tmp/plymouth_material_design_circle/ /usr/share/plymouth/themes/material_design_circle/
+update-alternatives --config default.plymouth
+update-initramfs -u
+reboot
+```
---
+++ default.nix
@@ -0,0 +1,93 @@
+{
+  outputs =
+    { self }:
+    {
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.boot.plymouthCircle;
+
+          plymouthTheme =
+            pkgs.runCommand "plymouth-theme-material-design-circle" { nativeBuildInputs = [ pkgs.ffmpeg ]; } ''
+              THEME_NAME="material_design_circle"
+              THEME_DIR="$out/share/plymouth/themes/$THEME_NAME"
+              mkdir -p $THEME_DIR/images
+              cp ${self}/theme.plymouth $THEME_DIR/$THEME_NAME.plymouth
+              cp ${self}/theme.script   $THEME_DIR/$THEME_NAME.script
+              cp ${cfg.logo}            $THEME_DIR/images/logo.png
+              for i in ${self}/images/*.png; do
+                ffmpeg -i "$i" \
+                  -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
+                  "$THEME_DIR/images/$(basename "$i")"
+              done
+            ''
+            + lib.optionalString cfg.circle.wavy ''
+              for i in ${self}/wave/*.png; do
+                ffmpeg -i "$i" \
+                  -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
+                  "$THEME_DIR/images/$(basename "$i")"
+              done
+            '';
+        in
+        {
+          options.boot.plymouthCircle = {
+            enable = lib.mkEnableOption "material design circle plymouth theme";
+
+            circle = {
+              wavy = lib.mkEnableOption "wavy style for circle";
+
+              red = lib.mkOption {
+                type = lib.types.ints.between 0 255;
+                default = 10;
+                description = "Red channel (0-255).";
+              };
+              green = lib.mkOption {
+                type = lib.types.ints.between 0 255;
+                default = 10;
+                description = "Green channel (0-255).";
+              };
+              blue = lib.mkOption {
+                type = lib.types.ints.between 0 255;
+                default = 10;
+                description = "Blue channel (0-255).";
+              };
+            };
+
+            logo = lib.mkOption {
+              type = lib.types.path;
+              default = "${self}/images/logo.png";
+              defaultText = lib.literalExpression ''"''${self}/images/logo.png"'';
+              description = "Path to a logo .png for the theme.";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            boot = {
+              consoleLogLevel = lib.mkDefault 0;
+              initrd = {
+                verbose = lib.mkDefault false;
+                systemd.enable = lib.mkDefault true;
+              };
+              plymouth = {
+                enable = lib.mkDefault true;
+                theme = "material_design_circle";
+                themePackages = [ plymouthTheme ];
+              };
+              kernelParams = [
+                "quiet"
+                "splash"
+                "udev.log_level=3"
+                "rd.udev.log_level=3"
+                "systemd.show_status=false"
+                "vt.global_cursor_default=0"
+              ];
+            };
+          };
+        };
+    };
+}
