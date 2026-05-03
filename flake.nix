262bc64a6255093174d5e0c51d916d803b6c06d4
strtab <nixjoyer@gmail.com>
2026-05-03 19:15:04 +0300

    fix

--- flake.nix
+++ flake.nix
@@ -11,29 +11,6 @@
         }:
         let
           cfg = config.boot.plymouthCircle;
-          plymouthTheme =
-            pkgs.runCommand "plymouth-theme-material-design-circle" { nativeBuildInputs = [ pkgs.ffmpeg ]; }
-              ''
-                THEME_NAME="material_design_circle"
-                THEME_DIR="$out/share/plymouth/themes/$THEME_NAME"
-                mkdir -p $THEME_DIR/images
-                cp ${self}/theme.plymouth $THEME_DIR/$THEME_NAME.plymouth
-                cp ${self}/theme.script   $THEME_DIR/$THEME_NAME.script
-                cp ${cfg.logo.path}       $THEME_DIR/images/logo.png
-                sed -i 's/^reduction.*/reduction = ${cfg.logo.scale}/' "$THEME_DIR/$THEME_NAME.script"
-                for i in ${self}/images/*.png; do
-                  ffmpeg -i "$i" \
-                    -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
-                    "$THEME_DIR/images/$(basename "$i")"
-                done
-                ${lib.optionalString cfg.circle.wavy ''
-                  for i in ${self}/wave/*.png; do
-                    ffmpeg -i "$i" \
-                      -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
-                      "$THEME_DIR/images/$(basename "$i")"
-                  done
-                ''}
-              '';
         in
         {
           options.boot.plymouthCircle = {
@@ -74,28 +51,55 @@
             };
           };
 
-          config = lib.mkIf cfg.enable {
-            boot = {
-              consoleLogLevel = lib.mkDefault 0;
-              initrd = {
-                verbose = lib.mkDefault false;
-                systemd.enable = lib.mkDefault true;
+          config = lib.mkIf cfg.enable (
+            let
+              plymouthTheme =
+                pkgs.runCommand "plymouth-theme-material-design-circle" { nativeBuildInputs = [ pkgs.ffmpeg ]; }
+                  ''
+                    THEME_NAME="material_design_circle"
+                    THEME_DIR="$out/share/plymouth/themes/$THEME_NAME"
+                    mkdir -p $THEME_DIR/images
+                    cp ${self}/theme.plymouth $THEME_DIR/$THEME_NAME.plymouth
+                    cp ${self}/theme.script   $THEME_DIR/$THEME_NAME.script
+                    cp ${cfg.logo.path}       $THEME_DIR/images/logo.png
+                    sed -i 's/^reduction.*/reduction = ${cfg.logo.scale}/' "$THEME_DIR/$THEME_NAME.script"
+                    for i in ${self}/images/*.png; do
+                      ffmpeg -i "$i" \
+                        -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
+                        "$THEME_DIR/images/$(basename "$i")"
+                    done
+                    ${lib.optionalString cfg.circle.wavy ''
+                      for i in ${self}/wave/*.png; do
+                        ffmpeg -i "$i" \
+                          -vf "colorchannelmixer=rr=${toString cfg.circle.red}/255:gg=${toString cfg.circle.green}/255:bb=${toString cfg.circle.blue}/255" \
+                          "$THEME_DIR/images/$(basename "$i")"
+                      done
+                    ''}
+                  '';
+            in
+            {
+              boot = {
+                consoleLogLevel = lib.mkDefault 0;
+                initrd = {
+                  verbose = lib.mkDefault false;
+                  systemd.enable = lib.mkDefault true;
+                };
+                plymouth = {
+                  enable = lib.mkDefault true;
+                  theme = "material_design_circle";
+                  themePackages = [ plymouthTheme ];
+                };
+                kernelParams = [
+                  "quiet"
+                  "splash"
+                  "udev.log_level=3"
+                  "rd.udev.log_level=3"
+                  "systemd.show_status=false"
+                  "vt.global_cursor_default=0"
+                ];
               };
-              plymouth = {
-                enable = lib.mkDefault true;
-                theme = "material_design_circle";
-                themePackages = [ plymouthTheme ];
-              };
-              kernelParams = [
-                "quiet"
-                "splash"
-                "udev.log_level=3"
-                "rd.udev.log_level=3"
-                "systemd.show_status=false"
-                "vt.global_cursor_default=0"
-              ];
-            };
-          };
+            }
+          );
         };
     };
 }
